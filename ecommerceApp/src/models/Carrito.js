5f804bf6fa5ff12573072c18cd936681e583e87d
Diego <redgo110.krextro@gmail.com>
2026-05-12 19:00:37 +0000

    se agrego logica de item carrito

--- ecommerceApp/src/models/Carrito.js
+++ ecommerceApp/src/models/Carrito.js
@@ -1,7 +1,13 @@
-class Carrito {
+import { agregarItemCarrito } from "../controllers/itemCarritoController.js";
+
+export class Carrito {
   constructor(id_carrito, usuario) {
     this.id_carrito = id_carrito;
     this.usuario = usuario;
     this.ItemCarrito = [];
+  }
+
+  agregarProducto(id_producto, cantidad, id_item) {
+    return agregarItemCarrito(this, id_producto, cantidad, id_item);
   }
 }
