e00bb361e59480700c10a82cc061220c392437bb
Rodrigo Emanuel Gomez Muñoz <emanuelmunoz695@gmail.com>
2026-05-19 19:01:13 -0300

    Agrego Sequelize a server.js y los controllers, models y routes de OrdenCompra y DetalleOrden

--- ecommerceApp/server.js
+++ ecommerceApp/server.js
@@ -1,25 +1,38 @@
-
 // server.js - El motor de nuestra aplicación
 import express from 'express';
+import sequelize from './src/config/database.js';
 import productsRouter from './src/routes/products.js';
 import userRouter from './src/routes/user.js';
+import ordenCompraRouter from './src/routes/ordenCompra.js';
+import detalleOrdenRouter from './src/routes/detalleOrden.js';
 
 const app = express();
 const PORT = 3000;
+
 // Le decimos al servidor que exponga públicamente los archivos de la carpeta "public"
 app.use(express.static('public'));
 app.use(express.json()); // Middleware para parsear JSON en las solicitudes
 
 app.use('/api', productsRouter);
 app.use('/api', userRouter);
+app.use('/api/ordenes', ordenCompraRouter);
+app.use('/api/detalles', detalleOrdenRouter);
 
 app.post('/api/checkout', (req, res) => {
-    const carritoRecibido = req.body; // Obtenemos el carrito enviado desde el cliente
-    console.log('Carrito recibido:', carritoRecibido); // Imprimimos el carrito en la consola para verificar su contenido
-    res.json({ message: 'Compra recibida exitosamente' }); // Respondemos con un mensaje de éxito
+    const carritoRecibido = req.body;
+
+    console.log('Carrito recibido:', carritoRecibido);
+
+    res.json({ message: 'Compra recibida exitosamente' });
 });
 
 // Encendemos el servidor
-app.listen(PORT, () => {
- console.log(`✅Servidor corriendo en http://localhost:${PORT}`);
+sequelize.sync()
+.then(() => {
+ app.listen(PORT, () => {
+  console.log(`✅Servidor corriendo en http://localhost:${PORT}`);
+ });
+})
+.catch((error) => {
+ console.log('❌Error de conexión:', error);
 });
